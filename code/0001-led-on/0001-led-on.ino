5b303767be563945ca9ec7c5b85960f5fc4828d5
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2024-05-03 20:43:22 +0200

    First example
    "GPIO on" example to introduce structure.

---
+++ code/0001-led-on/0001-led-on.ino
@@ -0,0 +1,17 @@
+// this is a comment
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // set LED1 on pin 2 to output
+  pinMode(2, OUTPUT);
+
+  // turn LED1 on
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // do nothing
+}
